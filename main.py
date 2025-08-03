43b4510e18633ae8cce979de02b7e5d8b81c5639
RaihanKabir277 <raihank0198@gmail.com>
2025-08-03 07:01:27 +0600

    argument with default values added here

--- main.py
+++ main.py
@@ -9,9 +9,18 @@ window.minsize(500, 300)
 
 # my_label = tkinter.Label(text="This is Kabir")  #should define text = " " neither its not work
 my_label = tkinter.Label(text="This is Kabir", font=("Arial", 18, "bold"))  
-my_label.pack()
+my_label.pack(side="bottom", expand=1)
+# my_label.pack()
 
+# --------Argument with default values----------
 
+# def fun(a=1, b=2, c=3):
+#     a = b
+#     b= c
+#     print(a)
+
+# fun()   #no need to give argument by default it will work perfectly 
+# fun(b=5)   #it will work also 
 
 
 window.mainloop()
